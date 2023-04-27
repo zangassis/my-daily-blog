77c78b5e7a18060c287cbf874250090b5c8773fa
Assis Zang <assis.zang@animaeducacao.com.br>
2023-04-27 18:13:37 -0300

    feat: Added css and new controller

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyDailyBlog.Data;
+
+namespace MyDailyBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly PostManager _manager;
+
+        public BlogController(PostManager manager)
+        {
+            _manager = manager;
+        }
+
+        // GET: BlogController/{idd}
+        [Route("blog/{id}")] // Set the ID parameter  
+        public ActionResult Index(Guid id)
+        {
+            var post = _manager.GetPostById(id);
+            return View(post);
+        }
+    }
+}
